b493520bbf7b231ef57fa1deb2e8f85aeaf7112f
Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
2023-11-25 21:00:05 +0900

    add: update user and delete user functions

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -73,7 +73,57 @@ const getAllUsers = async (req: Request, res: Response) => {
   }
 };
 
+const updateUser = async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.userId;
+    const userDataFromRequest = req.body;
+
+    const result = await UserServices.updateUserInDB(
+      userId,
+      userDataFromRequest,
+    );
+
+    res.status(200).json({
+      success: true,
+      message: 'User updated successfully!',
+      data: result,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(400).json({
+      success: false,
+      message: 'There was an Error in updating user data',
+      error: error,
+    });
+  }
+};
+
+const deleteAUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.id;
+    const result = await UserServices.deleteAUserFromDB(id);
+
+    res.status(200).json({
+      success: true,
+      message: 'User deleted successfully!',
+      data: null,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(400).json({
+      success: false,
+      message: 'There was an Error in deleting user',
+      error: error,
+    });
+  }
+};
+
 export const userControllers = {
   createUser,
   getASingleUser,
+  getAllUsers,
+  updateUser,
+  deleteAUser,
 };
