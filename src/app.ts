8ec63fcd8eae107f30e184a884808021155c8a23
Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
2023-11-25 20:41:00 +0900

    update: server setup complete and updated the user routes

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import { UserRoutes } from './modules/user/user.route';
+
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/users', UserRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server in running! Thanks for visiting');
+});
+
+export default app;
