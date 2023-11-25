8c1973b9fc4a5d2abaaf81cea03257292c4cf579
sakibulhasan0 <bb11021@b-jet.miyazaki-u.ac.jp>
2023-11-25 23:59:53 +0900

    add: oder related routes

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -15,4 +15,12 @@ router.put('/:userId', userControllers.updateUser);
 
 router.delete('/:userId', userControllers.deleteAUser);
 
+// Order Related Routes
+
+router.put('/:userId/orders/', userControllers.updateUserOrder);
+
+router.get('/:userId/orders/', userControllers.getOrderForUser);
+
+router.get('/:userId/orders/total-price', userControllers.getTotalPriceForUser);
+
 export const UserRoutes = router;
