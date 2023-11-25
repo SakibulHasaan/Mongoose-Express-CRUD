f83867f4900b6115284b82deafabfdfe961f0563
sakibulhasan0 <bb11021@b-jet.miyazaki-u.ac.jp>
2023-11-26 01:32:14 +0900

    add: update validation schema

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -35,3 +35,42 @@ export const UserValidationSchema = z.object({
   }),
   orders: z.array(OrderValidationSchema).optional(),
 });
+
+export const UserUpdateValidationSchema = z.object({
+  userId: z.number().positive('User ID must be a positive number').optional(),
+  username: z
+    .string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(15, 'Username must be at most 15 characters')
+    .optional(),
+  password: z
+    .string()
+    .min(6, 'Password must be at least 6 characters')
+    .optional(),
+  fullName: z
+    .object({
+      firstName: z
+        .string()
+        .min(1, 'First name is required')
+        .max(15, 'First name must be at most 15 characters')
+        .optional(),
+      lastName: z
+        .string()
+        .min(1, 'Last name is required')
+        .max(15, 'Last name must be at most 15 characters')
+        .optional(),
+    })
+    .optional(),
+  age: z.number().min(1, 'Age must be at least 1').optional(),
+  email: z.string().email('Invalid email format').optional(),
+  isActive: z.boolean().default(true).optional(),
+  hobbies: z.array(z.string()).optional(),
+  address: z
+    .object({
+      street: z.string().min(1, 'Street is required').optional(),
+      city: z.string().min(1, 'City is required').optional(),
+      country: z.string().min(1, 'Country is required').optional(),
+    })
+    .optional(),
+  orders: z.array(OrderValidationSchema).optional(),
+});
