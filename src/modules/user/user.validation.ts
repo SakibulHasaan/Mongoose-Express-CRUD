9e9a68a5413fbac1a69538f836673748876393cc
Md. Sakibul Hasan <54328877+SakibulHasaan@users.noreply.github.com>
2023-11-25 20:44:02 +0900

    add: validation schema for User type

---
+++ src/modules/user/user.validation.ts
@@ -0,0 +1,37 @@
+import { z } from 'zod';
+
+const OrderValidationSchema = z.object({
+  productName: z.string().min(1, 'Product name is required'),
+  price: z.number().min(1, { message: 'Price must be greater than 0' }),
+  quantity: z.number().int().positive('Quantity must be a positive integer'),
+});
+
+// Define the User schema using Zod with required error messages
+export const UserValidationSchema = z.object({
+  userId: z.number().positive('User ID must be a positive number'),
+  username: z
+    .string()
+    .min(3, 'Username must be at least 3 characters')
+    .max(15, 'Username must be at most 15 characters'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+  fullName: z.object({
+    firstName: z
+      .string()
+      .min(1, 'First name is required')
+      .max(15, 'First name must be at most 15 characters'),
+    lastName: z
+      .string()
+      .min(1, 'Last name is required')
+      .max(15, 'Last name must be at most 15 characters'),
+  }),
+  age: z.number().min(1, 'Age must be at least 1'),
+  email: z.string().email('Invalid email format'),
+  isActive: z.boolean().default(true),
+  hobbies: z.array(z.string()),
+  address: z.object({
+    street: z.string().min(1, 'Street is required'),
+    city: z.string().min(1, 'City is required'),
+    country: z.string().min(1, 'Country is required'),
+  }),
+  orders: z.array(OrderValidationSchema).optional(),
+});
